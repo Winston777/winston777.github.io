ce19dff95f94bf580021bab59a9a89f4ecc76b5f
Zaalem <nicolas.belleudy@ice-et.fr>
2018-09-27 16:45:00 +0200

    BetterMotherFuckingWebsite v1.2

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 This is my first coded website.
 v1.0 for motherfuckingsite
-v1.0 for bettermotherfuckingsite
+---------
+v1.0 for bettermotherfuckingsite | Creation
+v1.1 for bettermotherfuckingsite | Background color changed to gray
+v1.2 for bettermotherfuckingsite | Added JavaScript
+---------
+v1.0 for jscript.js | Creation
---
+++ jscript.js
@@ -0,0 +1,14 @@
+function init() {
+    var count=10;
+    var counter=setInterval(timer,1000);
+        function timer(){
+            count=count-1;
+            if(count==0){
+                 var answer = confirm("Veux tu découvrir le Palais des Délices?")
+if (answer) {window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";};
+if (answer = false)
+{txt = "Tant pis pour ta gueule!";}
+        return} 
+        }
+    }
+    (window.onload = init)
